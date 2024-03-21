e2eed600728bd83332d04b320e8790ec88a11841
devitoangelo <angelodevito4@gmail.com>
2024-03-21 17:50:55 +0100

    import img and AppMani and AppFooter

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+    name: 'AppFooter'
+}
+
+</script>
+<template>
+
+
+<footer id="site_footer">
+        <div class="container_footer">
+            <div class="content_footer">
+                <div class="top-2">
+                    <h3>Boolando s.r.l</h3>
+                    <section class="container_link">
+                        <ul class="link_footer">
+                            <li><a href="" class="link_decoration color_link">Informazioni legali</a></li>
+                            <li><a href="" class="link_decoration color_link">Informativa sulla privacy</a></li>
+                            <li><a href="" class="link_decoration color_link">Diritto di recesso</a></li>
+                        </ul>
+                    </section>
+                </div>
+
+
+
+
+            </div>
+            <div class="content_footer_app">
+                <div class="footer_app">
+                    <h5>Trovaci anche su </h5>
+                    <section class="link_app">
+                        <ul class="link_footer_app">
+                            <li><i class="fa-brands fa-facebook link_decoration p1"></i></li>
+                            <li><i class="fa-brands fa-facebook link_decoration p1"></i></li>
+                            <li><i class="fa-brands fa-instagram link_decoration p1"></i></li>
+                            <li><i class="fa-brands fa-facebook link_decoration p1"></i></li>
+                            <li><i class="fa-brands fa-youtube link_decoration p1"></i></li>
+                        </ul>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </footer>
+   
+
+</template>
+<style></style>
